69035a2dfa2d3015f514bfdafca985806e91db70
Jekri <jekri.orlina@any.tv>
2015-08-31 17:56:31 +0800

    Pseudo interface skeleton.

---
+++ index.js
@@ -0,0 +1,12 @@
+'use strict';
+
+var cudl = require('cuddle'),
+    mongo = require('anytv-node-mongo');
+
+exports.get_shortened_url_objects = function() {
+
+};
+
+exports.update_to_redirect_urls = function () {
+
+};
